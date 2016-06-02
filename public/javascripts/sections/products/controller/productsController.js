c8f5da4ca61ff09858ecae07c82dd9dea5b6ec21
Victor Rivas <vctr90w@gmail.com>
2016-06-01 19:04:32 -0500

    Product details added

--- public/javascripts/sections/products/controller/productsController.js
+++ public/javascripts/sections/products/controller/productsController.js
@@ -1,41 +1,41 @@
 angular.module('mainApp.controllerProducts',[])
 .controller('productsCtrl',['$scope','$sce','$getLocation','$getDataContents',function($scope,$sce,$getLocation,$getDataContents){
-	$scope.data = $getDataContents;
-	console.log($scope.data);
-	$getLocation.cLocator('products');
-	$scope.dTs = function(content) {
-		return $sce.trustAsHtml(content);
-	}
+  $scope.data = $getDataContents;
+  console.log($scope.data);
+  $getLocation.cLocator('products');
+  $scope.dTs = function(content) {
+    return $sce.trustAsHtml(content);
+  }
 }])
 .controller('soldCtrl',['$scope','$sce','$getLocation','$getDataContents',function($scope,$sce,$getLocation,$getDataContents){
-	$scope.data = $getDataContents;
-	console.log($scope.data);
-	$getLocation.cLocator('products');
-	$scope.dTs = function(content) {
-		return $sce.trustAsHtml(content);
-	}
+  $scope.data = $getDataContents;
+  console.log($scope.data);
+  $getLocation.cLocator('products');
+  $scope.dTs = function(content) {
+    return $sce.trustAsHtml(content);
+  }
 }])
 .controller('cortCtrl',['$scope','$sce','$getLocation','$getDataContents',function($scope,$sce,$getLocation,$getDataContents){
-	$scope.data = $getDataContents;
-	console.log($scope.data);
-	$getLocation.cLocator('products');
-	$scope.dTs = function(content) {
-		return $sce.trustAsHtml(content);
-	}
+  $scope.data = $getDataContents;
+  console.log($scope.data);
+  $getLocation.cLocator('products');
+  $scope.dTs = function(content) {
+    return $sce.trustAsHtml(content);
+  }
 }])
 .controller('sierCtrl',['$scope','$sce','$getLocation','$getDataContents',function($scope,$sce,$getLocation,$getDataContents){
-	$scope.data = $getDataContents;
-	console.log($scope.data);
-	$getLocation.cLocator('products');
-	$scope.dTs = function(content) {
-		return $sce.trustAsHtml(content);
-	}
+  $scope.data = $getDataContents;
+  console.log($scope.data);
+  $getLocation.cLocator('products');
+  $scope.dTs = function(content) {
+    return $sce.trustAsHtml(content);
+  }
 }])
 .controller('talCtrl',['$scope','$sce','$getLocation','$getDataContents',function($scope,$sce,$getLocation,$getDataContents){
-	$scope.data = $getDataContents;
-	console.log($scope.data);
-	$getLocation.cLocator('products');
-	$scope.dTs = function(content) {
-		return $sce.trustAsHtml(content);
-	}
+  $scope.data = $getDataContents;
+  console.log($scope.data);
+  $getLocation.cLocator('products');
+  $scope.dTs = function(content) {
+    return $sce.trustAsHtml(content);
+  }
 }]);
